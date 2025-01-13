05c36e0d379b0e0c44ba480b6a855fb23c5cf150
notExpectet <dercreeper6@gmail.com>
2025-01-13 20:52:26 +0100

    Update auto_update.py

--- auto_update.py
+++ auto_update.py
@@ -10,6 +10,11 @@ def update_repo():
     except subprocess.CalledProcessError as e:
         print(f"Fehler beim Aktualisieren des Repos: {e}")
 
+def restart_bot():
+    os.system("taskkill /f /im python.exe")  # Beendet alle Python-Prozesse
+    os.system("start python main.py")       # Startet den Bot neu
+
+
 if __name__ == "__main__":
     while True:
         update_repo()
